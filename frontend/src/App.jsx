374163c9c8322476018bdedabada382cba0cc9e4
ogstevyn <secondsteveoyebamiji@gmail.com>
2026-02-15 02:17:32 +0100

    app setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+const app = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-blue-600">
+        Banking Application
+      </h1>
+    </div>
+  )
+}
+
+export default app
